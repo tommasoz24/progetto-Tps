a70398bbd29a78d418a7ff35705e2a5465068f1b
tommaso zanatta <74731057+tommasoz24@users.noreply.github.com>
2022-12-17 22:55:41 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,75 +7,42 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
-/**
- * Classe che contiene il metodo main. Esecuzione di un programma lato client.
- * 
- * @autore Bartus
- *
- */
+
+
+// Client Main
 public class Main {
 
-	// static private boolean host = !true;
-	/**
-	 * Obiekt okna, które używane jest do rozgrywki
-	 * 
-	 */
 	static public OknoGra okno;
-	/**
-	 * Obiekt okna używanego do wyświetlania lobby
-	 * 
-	 */
+
 	static public JFrame oknoLobby;
-	/**
-	 *
-	 */
+
 	static public ClientThread clientThread; // thread cliente
-	/**
-	 * Obiekt wątku menedżera połączeń
-	 *
-	 */
-	static public SerwerLobbyConManager lobby;
-	/**
-	 * Lista działających wątków komunikacyjnych po stronie serwera
-	 *
-	 */
-	// static public List<SerwerLobbyListener> listeners = new ArrayList<>(10);
-	/**
-	 * Panel należący do okna lobby
-	 */
+
+	static public ServerLobbyConManager lobby;
+
 	static public PanelLobby pl;
-	/**
-	 * Nick gracza
-	 * 
-	 */
+
 	static public String name = "Gracz";
-	/**
-	 * IP/domena serwera
-	 * 
-	 */
+
 	static public String ip;
 
-	/**
-	 * Metoda main programu
-	 * 
-	 * @param args Brak użycia w programie
-	 */
+
 	public static void main(String[] args) {
 
 		try {
-			JTextField adres = new JTextField("localhost");
-			JTextField nick = new JTextField("Gracz");
+			JTextField localhost = new JTextField("localhost");
+			JTextField username = new JTextField("Gracz");
 			Object[] message = {
-					"Adres:", adres,
-					"Nick:", nick
+					"address:", localhost,
+					"username:", username
 			};
-			int option = JOptionPane.showConfirmDialog(null, message, "Połącz z serwerem",
+			int option = JOptionPane.showConfirmDialog(null, message, "Connessione al server",
 					JOptionPane.OK_CANCEL_OPTION);
 			if (option == JOptionPane.CANCEL_OPTION)
 				System.exit(0);
 			else {
-				ip = adres.getText();
-				name = nick.getText();
+				ip = localhost.getText();
+				name = username.getText();
 			}
 			Socket polaczenie = new Socket(ip, 2020);
 			clientThread = new ClientThread(polaczenie);
@@ -89,10 +56,9 @@ public class Main {
 			oknoLobby.setContentPane(pl);
 			oknoLobby.setVisible(true);
 		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			JOptionPane.showMessageDialog(null, "Nie udało się połączyć z serwerem");
+			JOptionPane.showMessageDialog(null, "Impossibile effettuare la connessione");
 		}
 	}
 
